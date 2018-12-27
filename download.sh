fafa5e02a22372b53e6553d3cd8799d6a50e8b30
peterwsetter <peter.w.setter@gmail.com>
2018-12-26 20:32:02 -0700

    fixed url for sdakota, deleted wrongly downloaded files

--- download.sh
+++ download.sh
@@ -10,7 +10,7 @@ wget https://www.ams.usda.gov/mnreports/nw_gr312.txt -O iowa/iowa$(date +%Y%m%d)
 wget https://www.ams.usda.gov/mnreports/jc_gr310.txt -O missouri/missouri$(date +%Y%m%d).txt
 wget https://www.ams.usda.gov/mnreports/sf_gr314.txt -O pipestone/pipestone$(date +%Y%m%d).txt
 wget https://www.ams.usda.gov/mnreports/sf_gr315.txt -O corsica/corsica$(date +%Y%m%d).txt
-wget https://www.ams.usda.gov/mnreports/sf_gr312.txt -O sdakota/sdakota$(date +%Y%m%d).txt
+wget https://www.ams.usda.gov/mnreports/sf_gr311.txt -O sdakota/sdakota$(date +%Y%m%d).txt
 wget https://www.ams.usda.gov/mnreports/am_gr310.txt -O texas/texas$(date +%Y%m%d).txt
 wget https://www.ams.usda.gov/mnreports/ag_gr310.txt -O utah/utah$(date +%Y%m%d).txt
 wget https://www.ams.usda.gov/mnreports/to_gr310.txt -O wyoming/wyoming$(date +%Y%m%d).txt
